9327cc0cf23338bb40d12039c93476f956975523
AluviiInc <scott@aluvii.com>
2024-02-28 01:22:14 -0700

    end of page 44

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,58 @@
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "redux",
+    url: "https://redux.js.org",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const title = "My Hacker Stories";
 
 function App() {
-
-  const list = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'redux',
-      url: 'https://redux.js.org',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5, 
-      objectID: 1,
-
-    }
-  ]
-
   return (
     <div>
-      <ul>
-        {list.map(item => <li key={item.objectID}>
-          
+      <h1>{title}</h1>
+      <Search />
+      <List />
+      <hr />
+    </div>
+  );
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map((item) => (
+        <li key={item.objectID}>
           <span> {item.title} </span>
           <span> {item.url} </span>
           <span> {item.author} </span>
-          
-          
-           </li>)}
-
-      </ul>
-    </div>
+        </li>
+      ))}
+    </ul>
   );
-  }
+}
+
+function Search() {
+  return (
+    <>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"></input>
+    </>
+  );
+}
 
 export default App;
-
 
 //why is item working and not list.title?  Shouldn't it be list.title because that is the name of the array?
